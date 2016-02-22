ccac70c8319d3b494d4e48c49034654c8ea10881
apackin <apackin@gmail.com>
2016-02-22 18:42:44 -0500

    set up to run with node server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ var express = require('express'),
     app = express(),
     path = require('path');
 
+// app.use(express.static(__dirname, '../browser')));
+app.use(express.static('./browser'));
+// app.use(express.static(__dirname, '../public')));
+app.use(express.static('./public'));
+
 app.get('/', function(req, res){
     res.sendfile(path.join(__dirname, '../browser/index.html'));
 });
