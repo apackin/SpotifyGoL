228285503e5d6a113b3a7772b07f5c4c4aaf8fa1
apackin <apackin@gmail.com>
2016-02-24 17:23:17 -0500

    added note controller

--- browser/js/nexusSetup.js
+++ browser/js/nexusSetup.js
@@ -1,44 +1,44 @@
- nx.onload = function(){
-      nx.colorize("#f5871f");
+nx.onload = function(){
+    nx.colorize("#f5871f");
 
-      matrix1.col = 16;
-      matrix1.row = noteNames.length;
-      matrix1.init();
-      matrix1.resize($("#Content").width(), 250);
-      matrix1.draw();
-      nx.startPulse();
+    matrix1.col = 16;
+    matrix1.row = noteNames.length;
+    matrix1.init();
+    matrix1.resize($("#Content").width(), 250);
+    matrix1.draw();
+    nx.startPulse();
+    $('#noteController').append(createNoteController());
+};
+$(function(){
+  new Interface.Slider({
+   name : "BPM",
+   min : 80,
+   max : 150,
+   value : Tone.Transport.bpm.value,
+   drag : function(val){
+     Tone.Transport.bpm.value = val;
+   }
+  });
 
-    };
-    $(function(){
-      new Interface.Slider({
-       name : "BPM",
-       min : 80,
-       max : 150,
-       value : Tone.Transport.bpm.value,
-       drag : function(val){
-         Tone.Transport.bpm.value = val;
-       }
-      });
+  new Interface.Button({
+    text : "Start",
+    activeText : "Stop",
+    type : "toggle",
+    key : 32, //spacebar
+    start : function(){
+      Tone.Transport.start();
+      loop.start();
+    },
+    end : function(){
+      matrix1.stop();
+      Tone.Transport.stop();
+      stopBlink();
+    },
+  });
 
-      new Interface.Button({
-        text : "Start",
-        activeText : "Stop",
-        type : "toggle",
-        key : 32, //spacebar
-        start : function(){
-          Tone.Transport.start();
-          loop.start();
-        },
-        end : function(){
-          matrix1.stop();
-          Tone.Transport.stop();
-          stopBlink();
-        },
-      });
-
-      Interface.Loader();
-      $(window).on("resize", function(){
-        matrix1.resize($("#Content").width(), 250);
-        matrix1.draw();
-      });
-    });
+  Interface.Loader();
+  $(window).on("resize", function(){
+    matrix1.resize($("#Content").width(), 250);
+    matrix1.draw();
+  });
+});
