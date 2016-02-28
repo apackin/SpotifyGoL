f79a8b79529a1514b3162ac8eb04dad6b1da23fe
apackin <apackin@gmail.com>
2016-02-27 20:41:50 -0700

    Working on startup

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,5 +1,6 @@
 var myBeat = angular.module('myBeat', ['ui.router', 'ngMessages']);
 
-myBeat.config(function($locationProvider){
+myBeat.config(function($locationProvider, $urlRouterProvider){
 	$locationProvider.html5Mode({enabled:true, requireBase:false});
+	$urlRouterProvider.when('/', '/sequencer');
 });
