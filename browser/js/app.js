37f4f205a8d22bcbd81310688eb53b01669cb216
apackin <apackin@gmail.com>
2016-03-11 17:33:43 -0500

    Able to change notes on Lead

--- browser/js/app.js
+++ browser/js/app.js
@@ -24,16 +24,44 @@ angular.module('myBeatApp', [])
         },
         link: function(scope) {
         	var matId = 'matrix' + scope.part;
-        	// var currentMatrix = window[matId];
+        	scope.noteOptions = ['A','B','C','D','E','F','G'];
+        	selectedKeyOctaves = ["3", "3", "3", "3", "3"];
+        	
+        	scope.notesInputs = [];
+
+        	setTimeout(numberOfRows, 500);
+
+        	scope.selectedANote = function (idx, note) {
+        		selectedKeyNotes[idx] = note;
+        		console.log(selectedKeyNotes);
+        	}
+
+     		function numberOfRows () {
+     			for (var i = 0; i < window[matId].row; i++) {
+     				addSelector();
+     			}
+     			scope.$digest();
+     		}
+
+     		function addSelector() {
+     			scope.notesInputs.push('A1');
+     		}
+
+     		function removeSelector() {
+     			scope.notesInputs.pop('A1');
+     		}
 
 	        scope.addRow = function () {
+	        	console.log(selectedDrumSamples);
 		        window[matId].row++;
 		        window[matId].draw();
+		        addSelector();
 		    }
 
 		    scope.removeRow = function () {
 		        window[matId].row--;
 		        window[matId].draw();
+		        removeSelector();
 		    }
 
 
